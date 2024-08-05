7c81b89daa3e192fd77fdcf7c434f9cb28f88764
Brian Guterl <brianguterl99@gmail.com>
2024-08-05 00:45:54 -0400

    fixed fetch and api keys

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import fetchGames from "../services/apiClient";
+import { url, key } from "../services/apiClient";
+import { Text } from "@chakra-ui/react";
 
 interface Game {
   id: number;
@@ -15,18 +16,32 @@ interface fetchGamesResponse {
 }
 
 const GameGrid = () => {
-  const [games, setGames] = useState([]);
-  const [error, setError] = useState([]);
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
-    const getGames = async () => {
-      const gameData = await fetchGames();
-      setGames(gameData.results);
+    const fetchGames = async () => {
+      try {
+        const res = await fetch(`${url}/games?key=${key}`);
+        const gameData: fetchGamesResponse = await res.json();
+        setGames(gameData.results);
+      } catch (err: any) {
+        setError(err.message);
+      }
     };
-    getGames();
-  });
+    fetchGames();
+  }, []);
 
-  return <div>GameGrid</div>;
+  return (
+    <>
+      {error && <Text>{error}</Text>}
+      <ul>
+        {games.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default GameGrid;
--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,8 +1,12 @@
-const url = "https://api.rawg.io/api";
-const key = "5f5273ec9f834deb8b1424f8afd75067";
+export const url = "https://api.rawg.io/api";
+export const key = "5f5273ec9f834deb8b1424f8afd75067";
 
 const fetchGames = async () => {
-  const res = await fetch(url);
+  const res = await fetch(url, {
+    headers: {
+      key,
+    },
+  });
   const gameData = await res.json();
   return gameData;
 };
