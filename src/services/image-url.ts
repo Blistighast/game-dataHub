04294297dd337495127c9eff9842f737f95d1758
Brian Guterl <brianguterl99@gmail.com>
2024-08-05 21:58:54 -0400

    optimized game images

---
+++ src/services/image-url.ts
@@ -0,0 +1,11 @@
+const getCroppedImageUrl = (url: string) => {
+  const target = "media/";
+  const index = url.indexOf(target) + target.length;
+  const startUrl = url.slice(0, index);
+  const endUrl = url.slice(index);
+  const croppedImageUrl = `${startUrl}crop/600/400/${endUrl}`;
+
+  return croppedImageUrl;
+};
+
+export default getCroppedImageUrl;
