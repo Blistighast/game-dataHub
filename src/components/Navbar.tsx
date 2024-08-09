74b75f9d19f920f190a8d64e82ba27208b5bb988
Brian Guterl <brianguterl99@gmail.com>
2024-08-08 20:53:23 -0400

    added search functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,12 +3,16 @@ import logo from "../assets/controllerLogoCropped.jpg";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
-const Navbar = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const Navbar = ({ onSearch }: Props) => {
   return (
     // chakra horizontal stack
     <HStack padding="10px">
       <Image src={logo} boxSize="60px" borderRadius="20px" padding=".25em" />
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   );
